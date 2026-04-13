c306a68599e7f46be97a70de3ec998c903bf52cd
Görkem Yençak <gorkemy@192.168.1.78>
2026-04-14 01:47:38 +0300

    market loader full pipeline

--- src/data/market_loader.py
+++ src/data/market_loader.py
@@ -1,19 +1,71 @@
+import pandas as pd
+
+from src.config.datasets_config import CURVE_CONFIG
+
 from src.data.fred_downloader import FredCurveDownloader
 
 class MarketLoader:
 
     def __init__(self):
         
-        self.curves = {}
+        self.raw_curves = {}
+        self.clean_curves = {}
     
+    # download raw curves
+    def download_curves(self):
 
-    def load_curve(
+        for curve in CURVE_CONFIG.keys():
+            loader = FredCurveDownloader(
+                curve_name = curve
+            )
+
+            self.raw_curves[curve] = loader.download()
+
+    # clean single curve
+    def _clean_curve(
             self,
-            curve_name
+            df
     ):
         
-        loader = FredCurveDownloader(curve_name = curve_name)
-        df = loader.download()
-        self.curves[curve_name] = df
+        df = df.copy()
+
+        # convert to numeric columns
+        df = df.apply(pd.to_numeric, errors = 'coerce')
+
+        # handling missing values (forward fill)
+        df = df.ffill()
+
+        # dropping empty rows
+        df = df.dropna(how = 'all')
 
         return df
+    
+    # clean all curves
+    def clean_all_curves(self):
+
+        for name, df_curve in self.raw_curves.items():
+            self.clean_curves[name] = self._clean_curve(df = df_curve)
+
+    # date aligment across all curves
+    def align_dates(self):
+
+        df_merged = (
+            pd.concat(
+                self.clean_curves.values(),
+                axis = 1,
+                keys = self.clean_curves.keys()
+            )
+            .ffill()
+            .dropna()
+        )
+
+        return df_merged
+        
+    # market loader pipeline
+    def loader_pipeline(self):
+        
+        self.download_curves()
+        self.clean_all_curves()
+        df_curves = self.align_dates()
+
+        return df_curves
